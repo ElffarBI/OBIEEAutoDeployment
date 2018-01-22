39ac0b350ea34fdc30ff2ddc7c340f2fe384526b
ElffarBI <joel@elffar.co.uk>
2018-01-22 16:22:26 +0000

    Add files via upload

---
+++ OBIEEdeployment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $# -gt 0 ]; then
+  TARGET_SERVER=$1
+  WEBLOGIC_USER=$2
+  WEBLOGIC_PWD=$3
+  WEBLOGIC_SRC_URL=$4 #t3://server_name:port_number
+  WEBLOGIC_TGT_URL=$5
+  WEBLOGIC_PORT=9502
+  CONNECTION_POOL_JSON_FILE=$1"ConnectionPools.json"
+  RPD_VARIABLES_JSON_FILE=$1"rpdvariables.json"
+  OBIEE_HOME="/u01/oracle/Middleware/Oracle_Home"
+  SCRIPT_HOME="/u01/downloads/exportDir"
+  DOMAIN_NAME="bi"
+  echo $TARGET_SERVER
+  echo "Export Target Connection Pools"
+  $OBIEE_HOME/user_projects/domains/$DOMAIN_NAME/bitools/bin/datamodel.sh listconnectionpool -SI ssi -U $WEBLOGIC_USER -P $WEBLOGIC_PWD -S $TARGET_SERVER -N $WEBLOGIC_PORT -V true -O $SCRIPT_HOME/$CONNECTION_POOL_JSON_FILE
+  echo "Export Target rpd Variables"
+  $OBIEE_HOME/user_projects/domains/$DOMAIN_NAME/bitools/bin/datamodel.sh listrpdvariables -SI ssi -U $WEBLOGIC_USER -P $WEBLOGIC_PWD -S $TARGET_SERVER -N $WEBLOGIC_PORT -O $SCRIPT_HOME/$RPD_VARIABLES_JSON_FILE
+
+  echo "Execute WLST Script and get BAR file from Source Server"
+  cd $OBIEE_HOME/oracle_common/common/bin
+  ./wlst.sh  $SCRIPT_HOME/backup_command.py $WEBLOGIC_USER $WEBLOGIC_PWD $WEBLOGIC_SRC_URL $WEBLOGIC_TGT_URL
+  $OBIEE_HOME/user_projects/domains/$DOMAIN_NAME/bitools/bin/datamodel.sh updateconnectionpool -C $SCRIPT_HOME/$CONNECTION_POOL_JSON_FILE -SI ssi -U $WEBLOGIC_USER -P $WEBLOGIC_PWD -S $TARGET_SERVER -N $WEBLOGIC_PORT
+  $OBIEE_HOME/user_projects/domains/$DOMAIN_NAME/bitools/bin/datamodel.sh updaterpdvariables -C $SCRIPT_HOME/$RPD_VARIABLES_JSON_FILE -U $WEBLOGIC_USER -P $WEBLOGIC_PWD -SI ssi -S $TARGET_SERVER -N $WEBLOGIC_PORT
+  echo "WLST Script completed"
+else
+    echo "Target Server parameter not provided."
+fi
---
+++ backup_command.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import time
+
+timestr = time.strftime("%Y%m%d")
+exportDir='/u01/downloads/exportDir'
+workDir='/u01/downloads/workDir'
+oracle_home='/u01/oracle/Middleware/Oracle_Home'
+domain_name='bi'
+print 'Connecting to '+sys.argv[3]
+
+connect(sys.argv[1],sys.argv[2],sys.argv[3]);
+print 'Connected to '+sys.argv[3]
+print 'Export BAR file from Source Server'
+expcmdstr='exportServiceInstance(\'%s/user_projects/domains/%s\',\'ssi\',\'%s\',\'%s\',timestr)' % (oracle_home,domain_name,workDir,exportDir)
+print 'Export Command String is: '+expcmdstr
+exec expcmdstr
+findstr="find "+exportDir+" -name \*.bar -type f | tail -1"
+print 'Find String is: '+findstr
+barfile=os.popen(findstr).read()
+g = os.popen("chmod 644 "+barfile)
+print 'Current BAR file is', barfile
+disconnect();
+
+connect(sys.argv[1],sys.argv[2],sys.argv[4]);
+print 'Connected to '+sys.argv[4]
+print 'Import BAR file to Target Server'
+findstr="find "+exportDir+" -name \*.bar -type f | tail -1"
+barfile=os.popen(findstr).read()
+print 'BAR file to be used for import is', barfile
+barfile=barfile.replace("\n","")
+cmdstr='importServiceInstance(\'%s/user_projects/domains/%s\',\'ssi\',\'%s\',true,false,false)' % (oracle_home,domain_name,barfile)
+print 'Command String is: '+cmdstr
+exec cmdstr
+disconnect();
+exit()
